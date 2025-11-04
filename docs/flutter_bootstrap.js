7883d022b41bbc6fac169243322bf3a097a64288
Zweycinder <killermusic1998@gmail.com>
2025-11-04 21:05:34 +0300

    adding the website

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"ddf47dd3ff96dbde6d9c614db0d7f019d7c7a2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "214180944"
+    serviceWorkerVersion: "1782384381"
   }
 });
