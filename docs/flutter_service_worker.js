fa9be5efb53fcaaa5db3e1048a7281aabf49f657
Zweycinder <killermusic1998@gmail.com>
2025-11-04 20:53:19 +0300

    adding the website

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -25,7 +25,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "6c704b2e9e60010cf6758d0db5dc2eed
 "canvaskit/skwasm_heavy.wasm": "8034ad26ba2485dab2fd49bdd786837b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
-"flutter_bootstrap.js": "259abcfc6e9a2071d794e95957326135",
+"flutter_bootstrap.js": "0b44d50e0f1b280904a1f63c7b9f174e",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
